c00284e4bdb4febd0103f236d8ea6061d6297451
jiajiechen <jiajiechen0319@gmail.com>
2017-06-13 01:47:58 +0000

    refactor

--- benchmark/python/sparse_op.py
+++ benchmark/python/sparse_op.py
@@ -7,94 +7,114 @@ import time
 import argparse
 
 from mxnet.base import check_call, _LIB
+from util import get_data, estimate_density
 
 parser = argparse.ArgumentParser(description="Benchmark sparse operators",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--num-omp-threads', type=int, default=1, help='number of omp threads to set in MXNet')
 args = parser.parse_args()
 
+# some data information
+kdda = {
+    'data_mini': 'kdda.t.mini',
+    'data_name': 'kdda.t',
+    'data_origin_name': 'kdda.t.bz2',
+    'url': "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/kdda.t.bz2",
+    'feature_dim': 20216830,
+    'm': 200,
+    'batch_size': [64]
+}
 
-def get_data(data_dir, data_name, url, data_origin_name):
-    if not os.path.isdir(data_dir):
-        os.system("mkdir " + data_dir)
-    os.chdir(data_dir)
-    if (not os.path.exists(data_name)):
-        import urllib
-        zippath = os.path.join(data_dir, data_origin_name)
-        urllib.urlretrieve(url, zippath)
-        # decompress
-        os.system("bzip2 -d %r" % data_origin_name)
-    os.chdir("..")
+avazu = {
+    'data_mini': 'avazu-app.t.mini',
+    'data_name': 'avazu-app.t',
+    'data_origin_name': 'avazu-app.t.bz2',
+    'url': "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/avazu-app.t.bz2",
+    'feature_dim': 1000000,
+    'm': 500,
+    'batch_size': [64, 128]
+}
 
 
-def test_dot_real(batch_size):
+def measure_cost(repeat, f, *args, **kwargs):
+    # start bench
+    start = time.time()
+    results = []
+    for i in range(repeat):
+        results.append(f(*args, **kwargs))
+    for result in results:
+        result.wait_to_read()
+    end = time.time()
+    diff = end - start
+    return diff / repeat
 
+
+def test_dot_real(data_dict):
     def get_iter(path, data_shape, batch_size):
-        #pdb.set_trace()
         data_train = mx.io.LibSVMIter(data_libsvm=path,
                                       data_shape=data_shape,
                                       batch_size=batch_size)
         data_iter = iter(data_train)
         return data_iter
 
-    # dataset info
     data_dir = os.path.join(os.getcwd(), 'data')
-    data_name = 'kdda.t'
-    data_origin_name = 'kdda.t.bz2'
-    url = "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/kdda.t.bz2"
-    feature_dim = 20216830
-    density = 0.0001866
-    path = os.path.join(data_dir, data_name)
 
-    get_data(data_dir, data_name, url, data_origin_name)
+    path = os.path.join(data_dir, data_dict['data_name'])
+    if not os.path.exists(path):
+        get_data(
+            data_dir,
+            data_dict['data_name'],
+            data_dict['url'],
+            data_dict['data_origin_name']
+        )
+        assert os.path.exists(path)
+    
+    k = data_dict['feature_dim']
+    m = data_dict['m']
+    density = estimate_density(path, data_dict['feature_dim'])
 
-    if not os.path.exists(os.path.join(path)):
-        raise Exception("Data is not prepared!")
+    mini_path = os.path.join(data_dir, data_dict['data_mini'])
+    if not os.path.exists(mini_path):
+        os.system("head -n 2000 %r > %r" % (path, mini_path))
+        assert os.path.exists(mini_path)
+    
+    print "Running Benchmarking on %r data" % data_dict['data_mini']
+    for batch_size in data_dict['batch_size']:  # iterator through different batch size of choice
+        # pdb.set_trace()
+        print "batch_size is %d" % batch_size
+        # model
+        data_shape = (k, )
+        train_iter = get_iter(mini_path, data_shape, batch_size)
+        weight = mx.nd.random_uniform(low=0, high=1, shape=(k, m))
 
-    size = float(os.path.getsize(path)) / (2**20)
-
-    # model
-    #batch_size = 128
-    data_shape = (feature_dim, )
-    train_iter = get_iter(path, data_shape, batch_size)
-
-    k = 500
-    weight = mx.nd.random_uniform(low=0, high=1, shape=(feature_dim, k))
-    weight.wait_to_read()
-
-    csr_data = []
-    dns_data = []
-    num_batch = 0
-    for batch in train_iter:
-        data = train_iter.getdata()
-        csr_data.append(data)
-        dns_data.append(data.to_dense())
-        num_batch += 1
-
-    results = []
-    start = time.time()
-    for d in csr_data:
-        results.append(mx.nd.dot(d, weight))
-    for res in results:
-        res.wait_to_read()
-    end = time.time()
-    t_sparse = (end - start) / batch_size
-
-    results = []
-    start = time.time()
-    for d in dns_data:
-        results.append(mx.nd.dot(d, weight))
-    for res in results:
-        res.wait_to_read()
-    end = time.time()
-    t_dense = (end - start) / batch_size
-
-    ratio = t_dense / t_sparse
-    print('density(%)\tn\tm\tk\tt_dense/t_sparse\tt_dense\tt_sparse')
-    fmt = "%0.6f\t%d\t%d\t%d\t%0.2f\t%0.4f\t%0.6f"
-    print(
-	fmt % (density * 100, batch_size, feature_dim, k, ratio, t_dense, t_sparse)
-    )
+        csr_data = []
+        dns_data = []
+        num_batch = 0
+        for batch in train_iter:
+            data = train_iter.getdata()
+            csr_data.append(data)
+            dns_data.append(data.to_dense())
+            num_batch += 1
+        bag_of_data = [csr_data, dns_data]
+        num_repeat = 5
+        costs = []
+        for d in bag_of_data:
+            weight.wait_to_read()
+            cost = 0.
+            count = 0
+            for d_batch in d:
+                d_batch.wait_to_read()
+                cost += measure_cost(num_repeat, mx.nd.dot, d_batch, weight)
+                count += 1
+            costs.append(cost/count)
+        t_sparse = costs[0]
+        t_dense = costs[1]
+        ratio = t_dense / t_sparse
+        print('density(%)\tn\tm\tk\tt_dense/t_sparse\tt_dense\tt_sparse')
+        fmt = "%0.4f\t\t%d\t%d\t%d\t%0.2f\t\t\t%0.4f\t%0.6f"
+        print(
+    	    fmt % (density * 100, batch_size, m, k, ratio, t_dense, t_sparse)
+        )
 
 
 def test_dot_synthetic():
@@ -155,7 +175,7 @@ def test_dot_synthetic():
         cost = measure_cost_forward_baseline(repeat, sp.spmatrix.dot, lhs_csr_sp, rhs_dns_np)
         costs_baseline.append(cost)
         ratio_baseline = costs_baseline[0] / costs_baseline[1]
-        fmt = "%0.1f\t\t%s\t%d\t%d\t%d\t%0.2f\t\t\t%0.2f\t%0.5f\t\t\t%0.2f\t%0.6f\t\t%0.5f"
+        fmt = "%0.1f\t\t%s\t%d\t%d\t%d\t%0.2f\t\t\t%0.2f\t%0.5f\t\t%0.2f\t\t\t\t%0.6f\t%0.5f"
         print(fmt % (density * 100, str(ctx), n, m, k, ratio, costs[0], costs[1],
                      ratio_baseline, costs_baseline[0], costs_baseline[1]))
 
@@ -184,7 +204,7 @@ def test_dot_synthetic():
         cost = measure_cost_backward_baseline(repeat, sp.spmatrix.dot, sp.spmatrix.transpose, lhs_csr_sp, rhs_dns_np)
         costs_baseline.append(cost)
         ratio_baseline = costs_baseline[0] / costs_baseline[1]
-        fmt = "%0.1f\t\t%s\t%d\t%d\t%d\t%0.2f\t\t\t%0.2f\t%0.5f\t\t\t%0.2f\t%0.6f\t\t%0.5f"
+        fmt = "%0.1f\t\t%s\t%d\t%d\t%d\t%0.2f\t\t\t%0.2f\t%0.5f\t\t%0.2f\t\t\t\t%0.6f\t%0.5f"
         print(fmt % (density * 100, str(ctx), n, m, k, ratio, costs[0], costs[1],
                      ratio_baseline, costs_baseline[0], costs_baseline[1]))
 
@@ -209,8 +229,8 @@ def test_dot_synthetic():
                 bench_dot_forward(m, k[i], n[i], den, ctx, num_repeat)
 
     print("dot_backward\tdot(csr.T, dns)")
-    print('density(%)\tcontext\tn\tm\tk\tt_sparse/t_dense\tt_dense\tt_sparse'
-          '\tt_scipy_sparse/t_scipy_dense\tt_scipy_dense\tt_scipy_sparse')
+    print('density(%)\tcontext\tn\tm\tk\tt_dense/t_sparse\tt_dense\tt_sparse'
+          '\tt_scipy_dense/t_scipy_sparse\tt_scipy_dense\tt_scipy_sparse')
     for i in range(2):
         for ctx in contexts:
             for den in density:
@@ -218,6 +238,6 @@ def test_dot_synthetic():
 
 if __name__ == "__main__":
     import pdb
-    #test_dot_real(batch_size=64)
-    #test_dot_real(batch_size=128)
+    test_dot_real(avazu)
+    test_dot_real(kdda)
     test_dot_synthetic()
--- benchmark/python/util.py
+++ benchmark/python/util.py
@@ -10,16 +10,16 @@ def get_data(data_dir, data_name, url, data_origin_name):
         import urllib
         zippath = os.path.join(data_dir, data_origin_name)
         urllib.urlretrieve(url, zippath)
-        # decompress
         os.system("bzip2 -d %r" % data_origin_name)
     os.chdir("..")
 
 
-def estimate_density():
+def estimate_density(DATA_PATH, feature_size):
     """sample 10 times of a size of 1000 for estimating the density of the sparse dataset"""
     if not os.path.exists(DATA_PATH):
         raise Exception("Data is not there!")
     density = []
+    P = 0.01
     for _ in xrange(10):
         num_non_zero = 0
         num_sample = 0
@@ -29,29 +29,5 @@ def estimate_density():
                     num_non_zero += len(line.split(" ")) - 1
                     num_sample += 1
         density.append(num_non_zero * 1.0 / (feature_size * num_sample))
-    return sum(density) * 100 / len(density)
+    return sum(density) / len(density)
 
-
-def slide_mini(data_name, new_name, sample_size=3000):
-    if os.path.exists(data_name):
-        with open(data_name, 'rb') as f:
-            new_f = open(new_name, 'wb')
-            for _ in xrange(sample_size):
-                new_f.write(f.readline())
-            new_f.close()
-
-
-if __name__ == '__main__':
-
-    data_dir = os.path.join(os.getcwd(), 'data')
-    data_name = 'kdda.t'
-    data_origin_name = 'kdda.t.bz2'
-                    url = "https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/kdda.t.bz2"
-                         feature_dim = 20216883
-                              density = 0.0001866
-                                   path = os.path.join(data_dir, data_name)
-    DATA_PATH = "data/kdda.t"
-    P = 0.01 #sampling proportion
-    feature_size = 20216830
-    random.seed(10002)
-    print estimate_density()
